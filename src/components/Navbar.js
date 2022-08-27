ef94db86d4fa42101e67466df44f3078bda77d41
Leo <ronald.mauritsson@gmail.com>
2022-08-27 23:52:25 +0300

    Added responsive front page

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,29 @@
+import Link from "next/link";
+import { BiMenuAltRight } from "react-icons/bi";
+import { RiCloseLine } from "react-icons/ri";
+
 const Navbar = () => {
-  return <div className="">This is the nav bar</div>;
+  return (
+    <div className="px-4 py-[10px]">
+      <div className="flex justify-between items-center">
+        <h1 className="font-aboreto text-[28px] font-normal text-white">LRK</h1>
+        <div className="block md:hidden text-[28px]">
+          <BiMenuAltRight />
+        </div>
+
+        <div className="hidden md:block space-x-4 text-[17px]">
+          <Link href="/">Home</Link>
+          <Link href="/about">About Leo</Link>
+          <Link href="/about">Work</Link>
+          <Link href="/about">Career</Link>
+          <Link href="/about">Community</Link>
+          <button className="bg-white text-black py-2 px-4 rounded-lg">
+            Let's talk
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
---
+++ src/pages/about.js
@@ -0,0 +1,5 @@
+const About = () => {
+  return <div>This is the about us</div>;
+};
+
+export default About;
